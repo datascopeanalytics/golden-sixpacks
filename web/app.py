ea73732349215c6837f6c2cfc47093ac54428693
Irmak Sirer <irmak.sirer@datascopeanalytics.com>
2016-03-04 19:39:51 -0600

    basic voting structure

--- web/app.py
+++ web/app.py
@@ -8,10 +8,25 @@ def home():
         "home.html"
     )
 
+
+poll_data = {
+    'question' : 'Who are you voting for in the category of The Collaborator?',
+    'fields'   : ['Bo', 'Irmak', 'Mike', 'Dean', 'Brian', 'Vlad', 'Mollie', 'Michael']
+    }
+
+@app.route('/vote')
+def vote():
+    return flask.render_template('vote.html', data=poll_data)
+
+@app.route('/save_vote')
+def poll():
+    vote = request.args.get('field')
+    return vote 
+
+
 if __name__ == '__main__':
     app.config.update(
         DEBUG=True,
     )
     app.run(host='0.0.0.0')
-
 
