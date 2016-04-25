168330ee62b8eda9f21dabca8f10be3b85033a64
Irmak Sirer <irmak.sirer@datascopeanalytics.com>
2016-04-25 18:23:47 -0500

    reset_fb fab command

---
+++ fabfile/reset_db.py
@@ -0,0 +1,23 @@
+"""
+Functions for provisioning environments with fabtools (eat shit puppet!)
+"""
+# standard library
+import sys
+import copy
+import os
+from distutils.util import strtobool
+
+# 3rd party
+import fabric
+from fabric.api import env, task, local, run, settings, cd, sudo, lcd
+
+# local
+import decorators
+import utils
+
+
+@task(default=True)
+@decorators.needs_environment
+def default():
+    run('mongo goldensixpacks --eval "db.dropDatabase()"')
+                
